a6e13dcf874ce15ade41978615a68d73f491c03c
Derek <ttz2000@gmail.com>
2020-10-11 20:51:22 +0800

    fucked up ftp.py

--- ftp.py
+++ ftp.py
@@ -1,5 +1,9 @@
 from ftplib import FTP
+dfsaafads;fkajds;fksa
+dsafkadsj;fdsajf;ksajf
+fkdsaljf;dsafj;adsfj;dsaf
 
+;kfdsa;jf
 with FTP() as ftp:
     ftp.connect(host="192.168.1.204", port=21)
     ftp.login(user="cam",passwd= "cam")
