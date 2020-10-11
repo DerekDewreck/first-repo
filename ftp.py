587ac8ca0b95a5f62181776eaf57990ac61504f1
Derek <ttz2000@gmail.com>
2020-10-11 20:36:28 +0800

    ftp.py v1

---
+++ ftp.py
@@ -0,0 +1,10 @@
+from ftplib import FTP
+
+with FTP() as ftp:
+    ftp.connect(host="192.168.1.204", port=21)
+    ftp.login(user="cam",passwd= "cam")
+    # ftp.set_pasv(True)
+    ftp.cwd("array1/cam/")
+    ftp.dir()
+    # r.close()
+    ftp.quit()
